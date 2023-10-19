473b02d7a3ff494c78563b5d72f791169fe765b5
hakun0 <brithon.saniko@gmail.com>
2023-10-19 05:58:43 -0700

    puo

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,9 +11,9 @@ int main(void)
 {
 	int a;
 
-	for (a = 'a'; a =< 'z'; a++)
+	for (a = 'a'; a <= 'z'; a++)
 		putchar(a);
-	for (a = 'A'; a=< 'Z'; a++)
+	for (a = 'A'; a <= 'Z'; a++)
 		putchar(a);
 	putchar('\n');
 
