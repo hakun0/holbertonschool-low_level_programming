0523ff52134b7abee9bec28ee91a937500460c3b
hakun0 <brithon.saniko@gmail.com>
2023-10-20 04:46:20 -0700

    ltrm

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+}
