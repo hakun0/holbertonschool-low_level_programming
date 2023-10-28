8c33d88505682c43d99934b47c251fc88c8a4bf5
hakun0 <brithon.saniko@gmail.com>
2023-10-27 19:47:14 -0700

    8pp

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_array - function
+ * @a: int ptr
+ * @n: int
+ */
+void	print_array(int *a, int n)
+{
+	int	i;
+
+	for (i = 0; x < n; i++)
+	{
+		printf("%d", a[i]);
+		if ((i + 1) < n)
+			printf(", ");
+	}
+	printf("\n");
+}
