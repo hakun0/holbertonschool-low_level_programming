00ba2a37e9036ec08823f4b2f2d32d8efcc8022c
hakun0 <brithon.saniko@gmail.com>
2023-10-29 13:55:38 -0700

    allllll2

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,70 +1,20 @@
 #include "main.h"
 
 /**
- * cap_char - function
- * @c: char
- *
- * Return: int
+ * print_chessboard - function
+ * @a: char ptr ptr
  */
-int	cap_char(char c)
-{
-	if (c == ' ')
-		return (1);
-	if (c == '\t')
-		return (1);
-	if (c == '\n')
-		return (1);
-	if (c == ',')
-		return (1);
-	if (c == ';')
-		return (1);
-	if (c == '.')
-		return (1);
-	if (c == '!')
-		return (1);
-	if (c == '"')
-		return (1);
-	if (c == '(')
-		return (1);
-	if (c == ')')
-		return (1);
-	if (c == '{')
-		return (1);
-	if (c == '}')
-		return (1);
-	return (0);
-}
-
-/**
- * cap_apply - function
- * @c: char
- * @f: int
- *
- * Return: char
- */
-char	cap_apply(char c, int f)
-{
-	if (f)
-	{
-		if ((c >= 'a') && (c <= 'z'))
-			return (c - ('a' - 'A'));
-	}
-	return (c);
-}
-
-/**
- * cap_string - function
- * @str: char ptr
- *
- * Return: char ptr
- */
-char	*cap_string(char *str)
+void	print_chessboard(char (*a)[8])
 {
 	int	x;
+	int	y;
 
-	if (str == 0)
-		return (0);
-	for (x = 0; str[x]; x++)
-		str[x] = cap_apply(str[x], x == 0 || cap_char(str[x - 1]));
-	return (str);
+	for (y = 0; y < 8; y++)
+	{
+		for (x = 0; x < 8; x++)
+		{
+			_putchar(a[y][x]);
+		}
+		_putchar('\n');
+	}
 }
