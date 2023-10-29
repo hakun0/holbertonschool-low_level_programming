922d45842dfcdcea9316ef69cd47786b4827244a
hakun0 <brithon.saniko@gmail.com>
2023-10-29 12:03:03 -0700

    0aa

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include <main.h>
+/**
+ * _memset - function
+ * @s: char ptr
+ * @b: char
+ * @n: unsigned int
+ *
+ * Return: char ptr
+ */
+char	*_memset(char *s, char b, unsigned int n)
+{
+	unsigned int	x;
+
+	if (s == 0)
+		return (0);
+	x = 0;
+	while (x < n)
+	{
+		s[x] = b;
+		x++;
+	}
+	return (s);
+}
