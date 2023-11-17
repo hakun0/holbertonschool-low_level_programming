4b5ce021e002ac1756ae97c42dbddc952f309011
hakun0 <brithon.saniko@gmail.com>
2023-11-17 09:23:49 -0800

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - structure
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ * description: structure containing three elements
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
