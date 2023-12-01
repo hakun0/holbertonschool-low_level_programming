a17b9c572d20d28feefe5745db0f4665e2d8e80e
hakun0 <brithon.saniko@gmail.com>
2023-12-01 14:59:23 -0800

    cc

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,28 +1,45 @@
 #include "lists.h"
-/**
- * add_dnodeint - Write a function that adds a new node at the beginning.
- * @head: pointer to pointer to list
- * @n: value of the node
- * Return: The adress of the new element
- */
 
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+/**
+ * add_dnodeint_new - function
+ * @n: const int
+ *
+ * Return: dlistint_t ptr
+*/
+dlistint_t	*add_dnodeint_new(const int n)
 {
-	dlistint_t *new_node;
-	
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == 0)
+	dlistint_t	*r;
+
+	r = (dlistint_t *) malloc(sizeof(dlistint_t));
+	if (r == 0)
+		return (0);
+	r->n = n;
+	return (r);
+}
+
+/**
+ * add_dnodeint - function
+ * @head: dlistint_t ptr ptr
+ * @n: const int
+ *
+ * Return: dlistint_t ptr
+*/
+dlistint_t	*add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t	*r;
+
+	if (head == 0)
+		return (0);
+	r = add_dnodeint_new(n);
+	if (r == 0)
+		return (0);
+	if (*head == 0)
+		*head = r;
+	else
 	{
-		return (NULL);
+		r->next = *head;
+		(*head)->prev = r;
+		*head = r;
 	}
-	
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = *head;
-	if (*head)
-	{
-		(*head)->prev = new_node;
-		*head = new_node;
-	}
-	return (new_node);
+	return (r);
 }
