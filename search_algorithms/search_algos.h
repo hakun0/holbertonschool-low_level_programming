a0bc3e5fd866cceb20cdb6f0a69b9058dc57470d
hakun0 <brithon.saniko@gmail.com>
2024-01-19 23:30:27 +0100

    searchtestt

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,16 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* ---------- Libraries ---------- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/* ---------- Prototypes ---------- */
+/* 0-linear.c */
+int linear_search(int *array, size_t size, int value);
+/* 1-binary.c */
+int binary_search(int *array, size_t size, int value);
+
+
+#endif
